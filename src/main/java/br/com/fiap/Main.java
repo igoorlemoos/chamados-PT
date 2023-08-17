39e77bcf17103359b269af0bd35308c1aaf479e6
IgoorLemoos <igor.x41@hotmail.com>
2023-08-16 22:51:39 -0300

    Persistindo Area e aplicando tratamento de exceções

--- src/main/java/br/com/fiap/Main.java
+++ src/main/java/br/com/fiap/Main.java
@@ -1,7 +1,48 @@
 package br.com.fiap;
 
+import br.com.fiap.domain.entity.Area;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+import javax.swing.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("oracle");
+        EntityManager manager = factory.createEntityManager();
+
+        boolean salvou = false;
+
+        do {
+            var area = new Area();
+            String nome = JOptionPane.showInputDialog("Nome da área");
+            String descricao = JOptionPane.showInputDialog("Descrição");
+
+            area.setNome(nome).setDescricao(descricao);
+
+            try {
+                manager.getTransaction().begin();
+                manager.persist(area);
+                manager.getTransaction().commit();
+
+                System.out.println(area);
+            } catch (Exception ex) {
+                String erro = """
+                        ERRO!
+                                            
+                        NÃO FOI POSSIVEL SALVAR OS DADOS: """ + ex.getMessage() + """
+                                            
+                        MOTIVO: """ + ex.getLocalizedMessage();
+
+                JOptionPane.showMessageDialog(null, erro);
+
+                salvou = true;
+            }
+        } while (!salvou);
+
+        manager.close();
+        factory.close();
     }
 }
---
+++ src/main/java/br/com/fiap/domain/entity/Area.java
@@ -0,0 +1,70 @@
+package br.com.fiap.domain.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "TB_AREA", uniqueConstraints = {
+        @UniqueConstraint(name = "UK_NM_AREA", columnNames = {"NM_AREA"})
+//        Para que 'nome' não se repita
+
+})
+public class Area {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SQ_AREA")
+    @SequenceGenerator(name = "SQ_AREA", sequenceName = "SQ_AREA")
+    @Column(name = "ID_AREA")
+    private Long id;
+
+
+    @Column(name = "NM_AREA", nullable = false)
+    private String nome;
+
+    @Column(name = "DS_AREA")
+    private String descricao;
+
+    public Area() {
+    }
+
+    public Area(Long id, String nome, String descricao) {
+        this.setId(id);
+        this.setNome(nome);
+        this.setDescricao(descricao);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Area setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Area setNome(String nome) {
+        this.nome = nome;
+        return this;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public Area setDescricao(String descricao) {
+        this.descricao = descricao;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "Area{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", descricao='" + descricao + '\'' +
+                '}';
+    }
+}
